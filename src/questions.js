a9dd8cce0d9c137ad86c4185afdc9d70c65c75dc
TadasL0 <119298439+TadasL0@users.noreply.github.com>
2023-01-16 18:41:51 +0200

    Questions list update 1.0

--- src/questions.js
+++ src/questions.js
@@ -1,5 +1,5 @@
 export function getRandomQuestion(inputText) {
-    var questions = ["What makes you think that way?", "How?", "Why?" , "Why do you think so?", "What makes you think so?", "So?", "What problem is this solving?, Why does this occur?", "What is preventing me from solving this problem?"];
+    var questions = ["What makes you think that way?", "How?", "Why?" , "Why do you think so?", "What makes you think so?", "So?", "What problem is this solving?", "Why does this occur?", "What is preventing me from solving this problem?"];
     var selectedQuestion = questions[Math.floor(Math.random()*questions.length)];
     return selectedQuestion;
 }
