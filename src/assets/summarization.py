5e000de437554c277d4fb254ff7051e373b81c89
TadasL0 <119298439+TadasL0@users.noreply.github.com>
2023-01-17 19:50:38 +0200

    Random gpt3 string removal

--- src/assets/summarization.py
+++ src/assets/summarization.py
@@ -5,7 +5,7 @@ openai.api_key = os.environ.get("API_KEY")
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Summarize this for a second-grade student:\n\nI",
+  prompt="Summarize this for a second-grade student:\n\n",
   temperature=0.7,
   max_tokens=256,
   top_p=1,
